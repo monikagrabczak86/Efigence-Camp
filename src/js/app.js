973d55ee4525fa8dcd8b53aaf2d9511dfbc2c8bc
Monika Grabczak <monikagrabczak86@gmail.com>
2018-08-27 23:32:57 +0200

    chart on dasboard site

--- src/js/app.js
+++ src/js/app.js
@@ -141,5 +141,74 @@
         const element = document.querySelector(".dashboard");
         element.classList.toggle("burger-active");
     });
+    
+//    chart - START
+    
+    var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+    
+//    chart - END
+    
+//    funkcja tymczasowa - START
+    
+    function chartLoad() {
+                const clientNumber = "efi";
+                const password = "camp";
+    
+                const formData = new FormData();
+                formData.append('login', clientNumber);
+                formData.append('password', password);
+    
+                const options = {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded'
+                    },
+                    data: formData,
+                    url: "https://efigence-camp.herokuapp.com/api/login",
+                };
+    
+                axios(options).then(function (response) {
+                    
+                    const optionsHistory = {
+                    method: 'GET',
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded'
+                    },
+                    url: "https://efigence-camp.herokuapp.com/api/data/history",
+                        };
+                        
+                        axios(optionsHistory).then(function (response) {
+                        console.log(response.data);
+                    
+                }).catch(function (error) {
+                    alert('Podane dane są nieprwidłowe');
+                })
+    
+            }
+                                    )};
+    
+        
+chartLoad();
+    
+//    funkcja tymczasowa - END
+    
 
 })();
